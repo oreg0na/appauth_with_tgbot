745e36b42e9ba1b9789389d2fca840dca82360c1
Slava Pogosov <pogoslav1@gmail.com>
2023-12-22 18:54:26 +0300

    add main files (1.0.0)

---
+++ telegrambot/__main__.py
@@ -0,0 +1,46 @@
+import asyncio
+import secrets
+
+from contextlib import suppress
+
+from aiogram import Router, Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from aiogram.enums import ParseMode
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from motor.core import AgnosticBase as MDB
+
+from pymongo.errors import DuplicateKeyError 
+
+router = Router()
+
+@router.message(CommandStart())
+async def start(message: Message, db: MDB) -> None:
+  with suppress(DuplicateKeyError):
+    await db.users.insert_one({
+      "_id": message.from_user.id,
+      "password": secrets.token_urlsafe(8),
+      "items": []
+    })
+  user = await db.users.find_one({"_id": message.from_user.id})
+  await message.reply(
+    f"Ваш ID: {hcode(user['_id'])}\n"
+    f"Ваш пароль: {hcode(user['password'])}\n\n"
+    f"<blockquote></blockquote>"
+  )
+
+async def main() -> None:
+  bot = Bot(..., parse_mode=ParseMode.HTML)
+  dp = Dispatcher()
+
+  cluster = AsyncIOMotorClient(host="localhost", port=27017)
+  db = cluster.appauthdb
+
+  dp.include_router(router)
+
+  await bot.delete_webhook(True)
+  await dp.start_polling(bot, db=db)
+
+if __name__ == "__main__":
+  asyncio.run(main())
